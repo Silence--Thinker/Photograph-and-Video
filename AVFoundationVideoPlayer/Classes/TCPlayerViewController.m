6fc8caae63928c39815635d3c0eee79e68056fd6
cxj15706 <cxj15706@LY.com>
2016-08-23 15:36:11 +0800

    基本功能完成

--- AVFoundationVideoPlayer/Classes/TCPlayerViewController.m
+++ AVFoundationVideoPlayer/Classes/TCPlayerViewController.m
@@ -54,13 +54,16 @@ static int TCPlayerViewControllerKVOContext = 0;
     self.playerView.playerLayer.player = self.player;
     
     // 设置资源URL
+//    NSURL *moviceURL = [[NSBundle mainBundle] URLForResource:@"movie" withExtension:@"mp4"];
     NSURL *moviceURL = [[NSBundle mainBundle] URLForResource:@"ElephantSeals" withExtension:@"mov"];
     self.asset = [AVURLAsset assetWithURL:moviceURL];
     
     // 每过 1s 监听时间 变化
     __weak typeof(self) weakSelf = self;
     _timeObserverToken = [self.player addPeriodicTimeObserverForInterval:CMTimeMake(1, 1) queue:dispatch_get_main_queue() usingBlock:^(CMTime time) {
+        // 定时刷新时间轴 跟 滑动滑块刷新 冲突 will TO DO
         weakSelf.timeSlider.value = CMTimeGetSeconds(time);
+        weakSelf.startTimeLabel.text = [weakSelf second2MinutesAndSecond:CMTimeGetSeconds(time)];
     }];
 }
 
@@ -84,11 +87,47 @@ static int TCPlayerViewControllerKVOContext = 0;
 // MARK: - config Subviews
 
 - (void)buildingSubViews {
+//    self.automaticallyAdjustsScrollViewInsets = NO;
+//    self.view.translatesAutoresizingMaskIntoConstraints = NO;
+    
     self.view.backgroundColor = [UIColor grayColor];
     TCPlayerView *playerView = [[TCPlayerView alloc] init];
-    playerView.frame = CGRectInset(self.view.bounds, 20, 30);
+    playerView.translatesAutoresizingMaskIntoConstraints = NO;
+    playerView.backgroundColor = [UIColor redColor];
     [self.view addSubview:playerView];
     self.playerView = playerView;
+    
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:playerView
+                                                          attribute:NSLayoutAttributeLeft
+                                                          relatedBy:NSLayoutRelationEqual
+                                                             toItem:self.view
+                                                          attribute:NSLayoutAttributeLeft
+                                                         multiplier:1.0
+                                                           constant:0]];
+    
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:playerView
+                                                          attribute:NSLayoutAttributeRight
+                                                          relatedBy:NSLayoutRelationEqual
+                                                             toItem:self.view
+                                                          attribute:NSLayoutAttributeRight
+                                                         multiplier:1.0
+                                                           constant:0]];
+    
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:playerView
+                                                          attribute:NSLayoutAttributeTop
+                                                          relatedBy:NSLayoutRelationEqual
+                                                             toItem:self.view
+                                                          attribute:NSLayoutAttributeTop
+                                                         multiplier:1.0
+                                                           constant:0]];
+    
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:playerView
+                                                          attribute:NSLayoutAttributeBottom
+                                                          relatedBy:NSLayoutRelationEqual
+                                                             toItem:self.view
+                                                          attribute:NSLayoutAttributeBottom
+                                                         multiplier:1.0
+                                                           constant:0]];
     
     CGFloat sliderX = 50;
     CGFloat sliderH = 20;
@@ -96,6 +135,7 @@ static int TCPlayerViewControllerKVOContext = 0;
     _timeSlider.backgroundColor = [UIColor redColor];
     [self.view addSubview:_timeSlider];
     _timeSlider.frame = CGRectMake(sliderX, SCREEN_HEIGHT - sliderH, SCREEN_WIDTH - 2 * sliderX, sliderH);
+    [_timeSlider addTarget:self action:@selector(timeSliderDidChange:) forControlEvents:UIControlEventTouchDown];
     
     _startTimeLabel = [[UILabel alloc] init];
     _startTimeLabel.font = [UIFont systemFontOfSize:11.0];
@@ -113,7 +153,29 @@ static int TCPlayerViewControllerKVOContext = 0;
     _durationTimeLabel.frame = CGRectMake(sliderX + CGRectGetWidth(_timeSlider.frame) - 50, CGRectGetMinY(_timeSlider.frame) - 20, 50, 20);
     _durationTimeLabel.text = @"0.00";
     
+    _playPauseButton = [[UIButton alloc] init];
+    [_playPauseButton setTitle:@"播放暂停" forState:UIControlStateNormal];
+    [_playPauseButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    _playPauseButton.titleLabel.font = [UIFont systemFontOfSize:12.0];
+    [self.view addSubview:_playPauseButton];
+    _playPauseButton.frame = CGRectMake(sliderX + 85, CGRectGetMinY(_timeSlider.frame) - 60, 60, 40);
+    [_playPauseButton addTarget:self action:@selector(playPauseButtonWasPressed:) forControlEvents:UIControlEventTouchUpInside];
     
+    _rewindButton = [[UIButton alloc] init];
+    [_rewindButton setTitle:@"后退" forState:UIControlStateNormal];
+    [_rewindButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    _rewindButton.titleLabel.font = [UIFont systemFontOfSize:12.0];
+    [self.view addSubview:_rewindButton];
+    _rewindButton.frame = CGRectMake(sliderX, CGRectGetMinY(_timeSlider.frame) - 60, 60, 40);
+    [_rewindButton addTarget:self action:@selector(rewindButtonWasPressed:) forControlEvents:UIControlEventTouchUpInside];
+    
+    _fastForwardButton = [[UIButton alloc] init];
+    [_fastForwardButton setTitle:@"前进" forState:UIControlStateNormal];
+    [_fastForwardButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    _fastForwardButton.titleLabel.font = [UIFont systemFontOfSize:12.0];
+    [self.view addSubview:_fastForwardButton];
+    _fastForwardButton.frame = CGRectMake(sliderX + 85 + 85, CGRectGetMinY(_timeSlider.frame) - 60, 60, 40);
+    [_fastForwardButton addTarget:self action:@selector(fastForwardButtonWasPressed:) forControlEvents:UIControlEventTouchUpInside];
 }
 
 // MARK: - Properties
@@ -196,6 +258,36 @@ static int TCPlayerViewControllerKVOContext = 0;
     }];
 }
 
+// MARK: - IBActions
+
+- (void)playPauseButtonWasPressed:(UIButton *)sender {
+    if (self.player.rate != 1.0) {
+        // not playing 2 play
+        if (CMTIME_COMPARE_INLINE(self.currentTime, ==, self.duration)) {
+            // at end so got back to begining
+            self.currentTime = kCMTimeZero;
+        }
+        // play will set rate 1.0
+        [self.player play];
+    }else {
+        // playing so pause
+        [self.player pause];
+    }
+}
+
+- (void)rewindButtonWasPressed:(UIButton *)sender {
+    self.rate = MAX(self.player.rate - 2.0 , -2.0);
+}
+
+- (void)fastForwardButtonWasPressed:(UIButton *)sender {
+    self.rate = MIN(self.player.rate + 2.0, 2.0);
+}
+
+- (void)timeSliderDidChange:(UISlider *)sender {
+    // CMTimeMakeWithSeconds(a, b) a当前时间,b每秒钟多少帧
+    self.currentTime = CMTimeMakeWithSeconds(sender.value, 1000);
+}
+
 // MARK: - KV Observation
 
 - (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSString *,id> *)change context:(void *)context {
@@ -207,11 +299,16 @@ static int TCPlayerViewControllerKVOContext = 0;
     
     if ([keyPath isEqualToString:kKeyPathForAsset]) {
         if (self.asset) {
+            NSLog(@"now begin loading resource ");
             // 到这一步视频就会 加载进来了。会有默认帧图
             [self asynchronouslyLoadURLAsset:self.asset];
         }
     }else if ([keyPath isEqualToString:kKeyPathForPlayerItemDuration]){
+        
         // update timeSlider and enable/disable controls when duration > 0.0
+        // when playItem Initial or update playItem is called.resource no change no called again
+        
+        NSLog(@"now currentTime is %0.2f", CMTimeGetSeconds(self.currentTime));
         
         NSValue *newDurationAsvalue = change[NSKeyValueChangeNewKey];
         CMTime newDuration = [newDurationAsvalue isKindOfClass:[NSValue class]] ? newDurationAsvalue.CMTimeValue : kCMTimeZero;
@@ -221,21 +318,25 @@ static int TCPlayerViewControllerKVOContext = 0;
         self.timeSlider.maximumValue = newDurationSeconds;
         self.timeSlider.value = hasValidDuration ? CMTimeGetSeconds(self.currentTime) : 0.0;
         
-        self.startTimeLabel.text = [self second2MinutesAndSecond:CMTimeGetSeconds(self.currentTime)];
+        
         self.durationTimeLabel.text = [self second2MinutesAndSecond:newDurationSeconds];
         
     }else if ([keyPath isEqualToString:kKeyPathForPlayerRate]){
+        
         double newRate = [change[NSKeyValueChangeNewKey] doubleValue];
         NSLog(@"the new rate is %0.2f", newRate);
         
     }else if ([keyPath isEqualToString:kKeyPathForPlayerItemStatus]){
+        
         NSNumber *newStatusAsNumber = change[NSKeyValueChangeNewKey];
         AVPlayerItemStatus newStatus = [newStatusAsNumber isKindOfClass:[NSNumber class]] ? newStatusAsNumber.integerValue : AVPlayerItemStatusUnknown;
         
+        NSLog(@"playItem status have change new status : %zd", newStatus);
         if (newStatus == AVPlayerItemStatusFailed) {
             [self handleErrorWithMessage:self.player.currentItem.error.localizedDescription error:self.player.currentItem.error];
         }
     }else {
+        
         [super observeValueForKeyPath:keyPath ofObject:object change:change context:context];
     }
 }
